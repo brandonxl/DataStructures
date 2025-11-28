b32c76a6568124e2c166129fcd27bf8710c8798f
brasnxl <bransmiguel13@gmail.com>
2025-11-28 11:50:28 -0600

    solved problem 3 from leetcode, im really proud since it was hard for me and i was able to come with a solution, now i will try to optimize it since according to leetcode its not the best so far

---
+++ src/Hash/LongestSubstringWithoutRepating3.java
@@ -0,0 +1,47 @@
+package Hash;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepating3{
+    public static void main(String[] args) {
+
+        String s = "pwwkew";
+        int result =  check(s);
+        System.out.println(result);
+    }
+
+    static int check(String s){
+        int maxCount = 0;
+        int aux = 0;
+        HashMap<Integer, Character> hashMap = new HashMap<>();
+        int i = 0, j = 0;
+
+        while(j < s.length()){
+            if(!hashMap.containsValue(s.charAt(j))){
+                hashMap.put(j, s.charAt(j));
+                aux ++;
+                if(aux > maxCount){
+                    maxCount = aux;
+                }
+                j++;
+            }else{
+                hashMap.remove(i, s.charAt(i));
+                aux--;
+                i++;
+                while(i < j){
+                    if(hashMap.containsValue(s.charAt(j))){
+                        hashMap.remove(i, s.charAt(i));
+                        aux--;
+                        i++;
+                    }else{
+                        hashMap.put(j, s.charAt(j));
+                        aux++;
+                        j++;
+                        break;
+                    }
+                }
+            }
+        }
+        return maxCount;
+    }
+}
